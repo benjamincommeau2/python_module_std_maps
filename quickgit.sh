164c246519bd202a5b70667368abc53244d4e64d
Benjamin <benjamincommeau2@gmail.com>
2019-12-28 17:04:46 -0700

    testing quickgit.sh script

--- quickgit.sh
+++ quickgit.sh
@@ -1,9 +1,10 @@
 echo "$1"
 echo "Is the above commit message correct?"
-options=("y" "n")
+options=("Type 1 for yes and enter." "Type 2 for no and enter.")
 select yn in "${options[@]}"; do
   case $yn in
-    y) git init; git add .; git commit -m "$1"; git push origin master; break;;
-    n) break;;
+    "Type 1 for yes and enter.") 
+      git init; git add .; git commit -m "$1"; git push origin master; break;;
+    "Type 2 for no and enter.") break;;
   esac
 done
